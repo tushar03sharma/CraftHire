7969d776e4affc583c2649ba3af5a57d479154d2
tushar03sharma <tushar50sharma1415@gmail.com>
2026-06-29 23:29:14 +0530

    feat(server): add authentication routes and middleware for token protection

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const authRoutes = require('./routes/authRoutes');
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', authRoutes);
+
 
 app.get('/api/health' , (req,res)=>{
     res.json({status:'ok', message:"Server is alive"})
